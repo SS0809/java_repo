2dc49c11dc31790e1f3eccf5f3fccf2a2a256a28
Saurabh Saraswat <saurabh45215@gmail.com>
2025-01-06 21:22:35 +0530

    basic ops

---
+++ basic_ops.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class basic_ops {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+        performOperations(num1, num2);
+        scanner.close();
+    }
+
+    private static void performOperations(double num1, double num2) {
+        // Add method 
+        // Subtract method 
+        // Multiply method 
+        // Divide method 
+    }
+}
