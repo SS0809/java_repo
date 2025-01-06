04ef5726eefa96184d37f06a095fc9c10717250d
Saurabh Saraswat <saurabh45215@gmail.com>
2025-01-06 21:30:15 +0530

    multiple feature added

--- basic_ops.java
+++ basic_ops.java
@@ -8,7 +8,10 @@ public class basic_ops {
         performOperations(num1, num2);
         scanner.close();
     }
-
+    private static void multiply(double num1, double num2) {
+        double result = num1 * num2;
+        System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + result);
+    }
     private static void performOperations(double num1, double num2) {
         // Add method 
         // Subtract method 
